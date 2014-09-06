f057052e3797ed8f4ac2a326d3d96bd3d56df75b
WhatsARanjit <whatsaranjit@gmail.com>
2014-09-06 13:57:28 -0400

    Checking for age and gsubbing title down to a search worthy term

--- get_shows.rb
+++ get_shows.rb
@@ -11,15 +11,26 @@ begin
   raise
 end
 
+# Get current time for delay comparison
+time = Time.now
+
 config_params['shows'].each do |show,hash|
+  showname = show.to_s
   baseurl  = hash['torrent_url'] ? hash['torrent_url'] : config_params['torrent_url']
   options  = hash['torrent_options'] ? hash['torrent_options'] : config_params['torrent_options']
-  url      = baseurl + '/' + URI::encode(show.to_s) + '/' + options
+  url      = baseurl + '/' + URI::encode(showname) + '/' + options
   dest_dir = hash['dest_dir'] ? hash['dest_dir'] : config_params['dest_dir']
+  delay    = hash['delay'] ? hash['delay'] : config_params['delay']
   open(url) do |rss|
     feed = RSS::Parser.parse(rss)
     feed.items.each do |item|
-      puts "Item #{item.title}"
+      pubdate = Date.parse "#{item.pubDate} (#{time.getlocal.zone})"
+      now     = Date.parse time.strftime('%a, %d %b %Y %X +0000 (%Z)')
+      age     = now - pubdate
+      if age > delay 
+        title = item.title.gsub(/\./, ' ').gsub(/(#{showname}[a-z0-9]+\b).*/i, '\1')
+        puts title
+      end
     end
   end
 end
