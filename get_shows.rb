d17e4ec9cb9fb71edb143ce8d8a2a1820579fd96
WhatsARanjit <whatsaranjit@gmail.com>
2014-09-07 17:21:30 -0400

    Removed debug puts

--- get_shows.rb
+++ get_shows.rb
@@ -22,11 +22,9 @@ config_params['shows'].each do |show,hash|
   baseurl  = hash['torrent_baseurl'] ? hash['torrent_baseurl'] : config_params['torrent_baseurl']
   url      = hash['torrent_url'] ? hash['torrent_url'] : config_params['torrent_url']
   url      = url.gsub(/%uri/, uri).gsub(/%showname/, URI::encode(showname)).gsub(/%options/, options)
-  #url     = baseuri + '/' + URI::encode(showname) + '/' + options
   dest_dir = hash['dest_dir'] ? hash['dest_dir'] : config_params['dest_dir']
   delay    = hash['delay'] ? hash['delay'] : config_params['delay']
   cmd      = hash['torrent_cmd'] ? hash['torrent_cmd'] : config_params['torrent_cmd']
-  puts url
   begin
     rss_check = open(url)
   rescue Exception
