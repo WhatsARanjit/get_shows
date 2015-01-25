cad677df32784ff5cd5633932989f8b90d8c8260
WhatsARanjit <whatsaranjit@gmail.com>
2015-01-24 20:45:19 -0500

    Minimized HTTP calls to one instead of two

--- get_shows.rb
+++ get_shows.rb
@@ -52,66 +52,59 @@ config_params['shows'].each do |show,hash|
   url          = url.gsub(/%uri/, uri).gsub(/%showname/, URI::encode(searchstring)).gsub(/%options/, options)
   puts url if debug
 
-  # Rescue a failure from a back URL or even a 404 if not torrent is available
+  # Rescue a failure from a bad URL or a 404 if not torrent is available
   begin
-    rss_check = open(url, headers)
+    open(url, headers) do |rss|
+      feed = RSS::Parser.parse(rss)
+      feed.items.each do |item|
+
+        # Make a user-friendly, repeatable version of the title
+        title = item.title.gsub(/\./, ' ').gsub(/(#{showname}\s?(?:S\d+)?(?:E\d+)?).*/i, '\1')
+        check = %x{ /bin/find #{dest_dir} -iname "*#{title}*" | /bin/grep -q "#{title}" }
+
+        # Don't do the following if the title is already in the done array
+        if $?.exitstatus.to_i > 0
+          puts title if debug
+          puts "Passes on disk check [#{$?.exitstatus.to_i}]: /bin/find #{dest_dir} -iname \"*#{title}*\" | /bin/grep -q \"#{title}\"" if debug
+
+          # Calculate the age of the torrent relative to the delay
+          pubdate = Date.parse "#{item.pubDate} (#{time.getlocal.zone})"
+          now     = Date.parse time.strftime('%a, %d %b %Y %X +0000 (%Z)')
+          age     = now - pubdate
+
+          # If the torrent is old enough
+          if (age > delay) and (age < max_age)
+            puts "Passes age check [#{age}]: #{now} - #{pubdate}" if debug
+
+            # Extract the magnet URL
+            begin
+              magnet = item.enclosure.to_s.match(/url="(.*)"/) [1]
+            rescue
+              puts "Warning: Could not find URL in #{item.enclosure.to_s}"
+            else
+              puts "\"#{item.title}\" available from #{magnet}" if nodown
+              doit = %x{ #{cmd} #{magnet} } unless nodown
+              if $?.exitstatus.to_i < 1
+
+                # If torrent add works, add to done array and output info
+                puts "Downloading \"#{item.title}\" from #{magnet}"
+                done << title
+
+              end unless nodown # END: checking if torrent command succeeded
+
+            end # END: Rescue to see if we could get a URL
+          end # END: Age test
+          puts "Fails age check [#{age}]: #{now} - #{pubdate}" if debug
+        end unless done.include? title # END: if file does not already exist
+
+        # Mock add to array if not actually downloaded
+        done << title if nodown
+
+      end # END: RSS iteration
+    end # END: open(url) vblock
   rescue Exception
     puts "Notice: No RSS Feed for show \"#{show}\"\n"
-    good = false
-  else
-    good = true 
   end
-
-  # Start parsing the RSS if a good URL
-  open(url, headers) do |rss|
-    feed = RSS::Parser.parse(rss)
-    feed.items.each do |item|
-
-      # Make a user-friendly,` repeatable version of the title
-      #title = item.title.gsub(/\./, ' ').gsub(/(#{showname}[a-z0-9]+\b).*/i, '\1')
-      title = item.title.gsub(/\./, ' ').gsub(/(#{showname}\s?(?:S\d+)?(?:E\d+)?).*/i, '\1')
-      check = %x{ /bin/find #{dest_dir} -iname "*#{title}*" | /bin/grep -q "#{title}" }
-
-      # Don't do the following if the title is already in the done array
-      if $?.exitstatus.to_i > 0
-        puts title if debug
-        puts "Passes on disk check [#{$?.exitstatus.to_i}]: /bin/find #{dest_dir} -iname \"*#{title}*\" | /bin/grep -q \"#{title}\"" if debug
-
-        # Calculate the age of the torrent relative to the delay
-        pubdate = Date.parse "#{item.pubDate} (#{time.getlocal.zone})"
-        now     = Date.parse time.strftime('%a, %d %b %Y %X +0000 (%Z)')
-        age     = now - pubdate
-
-        # If the torrent is old enough
-        if (age > delay) and (age < max_age)
-          puts "Passes age check [#{age}]: #{now} - #{pubdate}" if debug
-
-          # Extract the magnet URL
-          begin
-            magnet = item.enclosure.to_s.match(/url="(.*)"/) [1]
-          rescue
-            puts "Warning: Could not find URL in #{item.enclosure.to_s}"
-          else
-            puts "\"#{item.title}\" available from #{magnet}" if nodown
-            doit = %x{ #{cmd} #{magnet} } unless nodown
-            if $?.exitstatus.to_i < 1
-
-              # If torrent add works, add to done array and output info
-              puts "Downloading \"#{item.title}\" from #{magnet}"
-              done << title
-
-            end unless nodown # END: checking if torrent command succeeded
-
-          end # END: Rescue to see if we could get a URL
-        end # END: Age test
-        puts "Fails age check [#{age}]: #{now} - #{pubdate}" if debug
-      end unless done.include? title # END: if file does not already exist
-
-      # Mock add to array if not actually downloaded
-      done << title if nodown
-
-    end # END: Iterating through items
-  end if good # END: open(url) vblock
 end # END: Shows hash
 
 puts "==>End: #{Time.now}"
