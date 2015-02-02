e1e7df4f217d76cbe834fbdedc63d677a64529c6
WhatsARanjit <whatsaranjit@gmail.com>
2015-02-02 16:44:00 -0500

    Added no-color flag

--- get_shows.rb
+++ get_shows.rb
@@ -14,6 +14,9 @@ parser = OptionParser.new do|opts|
   opts.on('-n', '--no-download', 'Reports new links without beginning download') do |value|
     options[:nodown] = true
   end
+  opts.on('-c', '--no-color', 'Removes color output') do |value|
+    options[:nocolor] = true
+  end
   opts.on('-h', '--help', 'Displays help') do |help|
     puts opts
     exit
@@ -21,26 +24,31 @@ parser = OptionParser.new do|opts|
 end
 parser.parse!
 
-debug  = options[:debug]
-nodown = options[:nodown]
+debug     = options[:debug]
+nodown    = options[:nodown]
+$coloroff = options[:nocolor]
+
+def outputs (string, color, coloroff = $coloroff)
+  output = coloroff ? string: string.colorize(color.to_sym)
+end
 
 # Get current time for delay comparison
 time = Time.now
 done = []
-puts "=>Start: #{time}".green
+puts outputs("=>Start: #{time}", 'green')
 
 # Load configuration file
 begin
   config_params = YAML.load_file('/etc/get_shows.yaml')
 rescue 
   $stderr.print "Error: Could not find file /etc/get_shows.yaml\n".red
-  puts "=>End: #{Time.now}".red
+  puts outputs("=>End: #{Time.now}", 'red')
   exit 1
 end
 
 config_params['shows'].each do |show,hash|
   showname = show.to_s
-  puts "==>#{showname}".cyan if debug
+  puts outputs("==>#{showname}", 'cyan') if debug
   uri          = hash['torrent_uri']     ? hash['torrent_uri']      : config_params['torrent_uri']
   options      = hash['torrent_options'] ? hash['torrent_options']  : config_params['torrent_options']
   url          = hash['torrent_url']     ? hash['torrent_url']      : config_params['torrent_url']
@@ -53,7 +61,7 @@ config_params['shows'].each do |show,hash|
   cmd          = hash['torrent_cmd']     ? hash['torrent_cmd']      : config_params['torrent_cmd']
 
   url          = url.gsub(/%uri/, uri).gsub(/%showname/, URI::encode(searchstring)).gsub(/%options/, options)
-  puts "==>#{url}".cyan if debug
+  puts outputs("==>#{url}", 'cyan') if debug
 
   # Rescue a failure from a bad URL or a 404 if not torrent is available
   begin
@@ -67,7 +75,7 @@ config_params['shows'].each do |show,hash|
         
         # Don't do the following if the title is already in the done array
         if Dir.glob(dest_file).empty?
-          puts "===>#{title}".cyan if debug
+          puts outputs("===>#{title}", 'cyan') if debug
 
           # Calculate the age of the torrent relative to the delay
           pubdate = Date.parse "#{item.pubDate} (#{time.getlocal.zone})"
@@ -76,42 +84,42 @@ config_params['shows'].each do |show,hash|
 
           # If the torrent is old enough
           if (age > delay) and (age < max_age)
-            puts "===>Passes age check [#{age}]: #{now} - #{pubdate}".cyan if debug
+            puts outputs("===>Passes age check [#{age}]: #{now} - #{pubdate}", 'cyan') if debug
 
             # Extract the magnet URL
             begin
               magnet = item.enclosure.to_s.match(/url="(.*)"/) [1]
             rescue
-              puts "===>Warning: Could not find URL in #{item.enclosure.to_s}".yellow
+              puts outputs("===>Warning: Could not find URL in #{item.enclosure.to_s}", 'yellow')
             else
-              puts "===>\"#{item.title}\" available from #{magnet}".green if nodown
+              puts outputs("===>\"#{item.title}\" available from #{magnet}", 'green') if nodown
               doit = %x{ #{cmd} #{magnet} } unless nodown
               if $?.exitstatus.to_i < 1
 
                 # If torrent add works, add to done array and output info
-                puts "===>Downloading \"#{item.title}\" from #{magnet}".green
+                puts outputs("===>Downloading \"#{item.title}\" from #{magnet}", 'green')
                 done << title
 
               end unless nodown # END: checking if torrent command succeeded
 
             end # END: Rescue to see if we could get a URL
           else
-            puts "===>Fails age check [#{age}]: #{now} - #{pubdate}".cyan if debug
+            puts outputs("===>Fails age check [#{age}]: #{now} - #{pubdate}", 'cyan') if debug
           end # END: Age test
         else
-          puts "===>Fails disk check: #{Dir.glob(dest_file)}".cyan if debug
+          puts outputs("===>Fails disk check: #{Dir.glob(dest_file)}", 'cyan') if debug
         end unless done.include? title # END: if file does not already exist
 
         # Mock add to array if not actually downloaded
         done << title if nodown
 
       end # END: RSS iteration
-      puts "==>Done with feed".green
+      puts outputs("==>Done with feed", 'green')
     end # END: open(url) vblock
   rescue Exception => e
     raise e.message
-    puts "==>Notice: No RSS Feed for show \"#{show}\"\n".green
+    puts outputs("==>Notice: No RSS Feed for show \"#{show}\"\n", 'green')
   end
 end # END: Shows hash
 
-puts "=>End: #{Time.now}".green
+puts outputs("=>End: #{Time.now}", 'green')
